ec5bbeb0d323675a3e3f12ab3aa3093c1d896c1f
RichEwin <richardewinlambert@gmail.com>
2020-08-29 10:19:46 +0100

    RED-Ammends the lower frequency, if soundwave is below lower threshold

--- spec/band_pass_filter_spec.rb
+++ spec/band_pass_filter_spec.rb
@@ -4,4 +4,8 @@ describe ('#filter') do
     it 'Returns the soundwave if frequencies are within range' do 
         expect(filter([100], 50, 2000)).to eq [100]
     end 
+
+    it 'Ammends the lower frequency, if soundwave is below lower threshold' do 
+        expect(filter([10], 50, 2000)).to eq [50]
+    end 
 end 
