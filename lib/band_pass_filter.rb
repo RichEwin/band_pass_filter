36e42815a3d126a3ae5c5339c4287bfb63c6f986
RichEwin <richardewinlambert@gmail.com>
2020-08-29 10:32:52 +0100

    GREEN-Ammends the higher frequency, if soundwave is above higher threshold

--- lib/band_pass_filter.rb
+++ lib/band_pass_filter.rb
@@ -3,6 +3,9 @@ def filter(soundwave, low_freq, high_freq)
 
     if soundwave.first < low_freq
         filter_input << low_freq
+    elsif 
+        soundwave.first > high_freq
+        filter_input << high_freq
     else 
         filter_input << soundwave.first
     end 
