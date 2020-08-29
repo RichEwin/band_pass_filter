7e34607de7a118610c27178a27bed0c438db3d14
RichEwin <richardewinlambert@gmail.com>
2020-08-29 10:43:14 +0100

    GREEN-Raises an error if soundwave file is corrupt

--- lib/band_pass_filter.rb
+++ lib/band_pass_filter.rb
@@ -2,6 +2,10 @@ def filter(soundwave, low_freq=100, high_freq=300)
     
     soundwave.map do | frequency |
 
+    unless frequency.is_a?(Numeric)
+        raise 'FILE IS CORRUPT'
+    end 
+
     if frequency < low_freq
         low_freq
     elsif 
